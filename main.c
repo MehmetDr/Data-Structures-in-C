050c4152a8f1e7b6efb5e503690aab907e335fdc
Mehmet Dogruer <mehmetdogruer38@gmail.com>
2025-11-21 14:30:15 +0300

    Add to Tail Function

--- main.c
+++ main.c
@@ -17,10 +17,30 @@ void addToHead(int data) {
     head = newNode;
 }
 
+void addToTail(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->next = NULL;
+
+    if (head == NULL) {
+        head = temp;
+        return;
+    }
+
+    struct Node* iter = head;
+    while (iter->next != NULL) {
+        iter = iter->next;
+    }
+
+    iter->next = temp;
+}
+
 
 int main() {
     addToHead(10);
     addToHead(20);
+    addToTail(30);
+    addToTail(40);
   
 
     return 0;
